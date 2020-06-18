1745e02069a90078baba19e118440a1aa24381c5
Electro Games <34932094+ElectroGamesYT@users.noreply.github.com>
2020-06-18 16:57:08 -0500

    Update Main.php

--- src/Electro/SimpleHeal/Main.php
+++ src/Electro/SimpleHeal/Main.php
@@ -18,22 +18,11 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener{
-
-	      public function onLoad(){
-                    $this->getLogger()->info("Plugin Loading");
-          }
-          public function onEnable(){
-     				$this->getServer()->getPluginManager()->registerEvents($this,$this);
-     				$this->getLogger()->info("Plugin Enabled");
-          }
-          public function onDisable(){
-                    $this->getLogger()->info("Plugin Disabled");
-          }
-
+	
           public function onCommand(CommandSender $sender,Command $cmd,string $label,array $args) : bool{
 		  if($cmd->getName() == "heal"){
     		 $sender->setHealth(20);
-    		 $sender->sendMessage(TextFormat::BOLD."You Have Been Healed!");
+    		 $sender->sendMessage(TextFormat::BOLD."§l§cYou Have Been Healed!");
           }
           return true;
 
