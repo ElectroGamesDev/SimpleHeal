5270409a9727a6844ea4c073539b6ddaa22c1b5f
Electro Games <2cool4u567@gmail.com>
2021-12-02 20:02:12 -0600

    Update to api 4

--- src/Electro/SimpleHeal/Main.php
+++ src/Electro/SimpleHeal/Main.php
@@ -2,10 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Electro\SimpleHeal;
+namespace Electro\SimpleFeed;
 
-use pocketmine\Server;
-use pocketmine\Player;
+use pocketmine\player\Player;
 
 use pocketmine\plugin\PluginBase;
 
@@ -13,29 +12,26 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-
-use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener{
 	
           public function onCommand(CommandSender $sender,Command $cmd,string $label,array $args) : bool{
            if ($sender instanceof Player) {
-               if ($cmd->getName() == "heal") {
+               if ($cmd->getName() == "feed") {
                    if (isset($args[0])){
-                       $player = $this->getServer()->getPlayer($args[0]);
+                       $player = $this->getServer()->getPlayerExact($args[0]);
                        if ($player){
-                           $player->setHealth(20);
-                           $player->sendMessage("§aYou Have Been Healed!");
-                           $sender->sendMessage("§aYou Have Healed " . $args[0] . "!");
+                           $player->getHungerManager()->setFood(20);
+                           $player->sendMessage("§aYou Have Been Fed!");
+                           $sender->sendMessage("§aYou Have Fed " . $args[0] . "!");
                        }
                        else{
                            $sender->sendMessage("§cThis player does not exist");
                        }
                    }
                    else {
-                       $sender->setHealth(20);
-                       $sender->sendMessage("§aYou Have Been Healed!");
+                       $sender->getHungerManager()->setFood(20);
+                       $sender->sendMessage("§aYou Have Been Fed!");
                    }
                }
            }
@@ -45,5 +41,4 @@ class Main extends PluginBase implements Listener{
           return true;
 
 		}
-
 }
